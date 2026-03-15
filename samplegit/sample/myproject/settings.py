d5f8f5b6aea9d7569a5e2681ea99a0dca61af8e0
Kaviyahari640 <kaviyakaviyak640@gmail.com>
2026-03-15 10:33:21 +0530

    new branch

---
+++ samplegit/sample/myproject/settings.py
@@ -0,0 +1,10 @@
+INSTALLED_APPS=[
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contib.contenttype',
+    'django.contrib.session',
+    'django.contrib.message',
+    'django.contrib.staticfiles',
+    'accounts',#samplegit
+]
+
